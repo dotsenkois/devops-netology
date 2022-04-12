7f48f85225da9c0cd3a0cf0919cced0003863261
dotsenkois <dotsenkois@yandex.ru>
2022-04-12 11:18:46 -0400

    [09-ci-04-jenkins] terraform + ansible. fix

--- 09-ci-04-jenkins/terraform/ansible.tf
+++ 09-ci-04-jenkins/terraform/ansible.tf
@@ -10,7 +10,7 @@ resource "null_resource" "wait" {
 
 resource "null_resource" "jenkins" {
   provisioner "local-exec" {
-    command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i infrastructure/inventory/cicd/hosts.yml infrastructure/site.yml"
+    command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ../infrastructure/inventory/cicd/hosts.yml ../infrastructure/site.yml"
   }
 
   depends_on = [
