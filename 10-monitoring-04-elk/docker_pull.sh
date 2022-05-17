f9852bffd3cb4a68322f1025d74ab488180313aa
dotsenkois <dotsenkois@yandex.ru>
2022-05-17 11:00:12 -0400

    [11-microservices-01-intro] init

--- 10-monitoring-04-elk/docker_pull.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker pull docker.elastic.co/elasticsearch/elasticsearch:7.11.0
-docker pull docker.elastic.co/kibana/kibana:7.11.0
-docker pull docker.elastic.co/logstash/logstash:6.3.2
-docker pull docker.elastic.co/beats/filebeat:7.2.0
-docker pull library/python:3.9-alpine
