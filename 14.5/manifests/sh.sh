a1e4458d36c2b860e8a3b2281ea1436e3e6bf3a1
dotsenkois <dotsenkois@yandex.ru>
2022-08-24 10:05:35 +0700

    [14.5] sh.sh

--- 14.5/manifests/sh.sh
+++ 14.5/manifests/sh.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
 echo "From frontend"
-kubectl exec frontend-c74c5646c-dmxfh -- curl -s -m 1 frontend
-kubectl exec frontend-c74c5646c-dmxfh -- curl -s -m 1 cache
-kubectl exec frontend-c74c5646c-dmxfh -- curl -s -m 1 backend
+kubectl exec frontend-c74c5646c-c4tbz -- curl -s -m 1 frontend
+kubectl exec frontend-c74c5646c-c4tbz -- curl -s -m 1 backend
 
 echo "From backend"
-kubectl exec backend-869fd89bdc-fwck8 -- curl -s -m 1 frontend
-kubectl exec backend-869fd89bdc-fwck8 -- curl -s -m 1 cache
-kubectl exec backend-869fd89bdc-fwck8 -- curl -s -m 1 backend
-
-echo "From cache"
-kubectl exec cache-b7cbd9f8f-95mt4 -- curl -s -m 1 frontend
-kubectl exec cache-b7cbd9f8f-95mt4 -- curl -s -m 1 cache
-kubectl exec cache-b7cbd9f8f-95mt4 -- curl -s -m 1 backend
+kubectl exec backend-869fd89bdc-dppgm -- curl -s -m 1 frontend
+kubectl exec backend-869fd89bdc-dppgm -- curl -s -m 1 backend
