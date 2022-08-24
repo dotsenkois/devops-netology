7b25317329ed78b81834b617322a1b9fccac4cb5
dotsenkois <dotsenkois@yandex.ru>
2022-08-24 10:31:56 +0700

    [14.5] policies

--- 14.5/manifests/sh.sh
+++ 14.5/manifests/sh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "From frontend"
-kubectl exec frontend-c74c5646c-c4tbz -- curl -s -m 1 http://curl.org
+kubectl exec frontend-c74c5646c-c4tbz -- curl -s -m 1 http://curl.org 
 kubectl exec frontend-c74c5646c-c4tbz -- curl -s -m 1 backend
 
 echo "From backend"
