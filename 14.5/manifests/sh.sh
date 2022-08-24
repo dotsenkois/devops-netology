c738ded89788fcce3add19d79da687b87930c631
dotsenkois <dotsenkois@yandex.ru>
2022-08-24 13:16:58 +0700

    [14.5] sh

--- 14.5/manifests/sh.sh
+++ 14.5/manifests/sh.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "From frontend"
-kubectl exec frontend-c74c5646c-c4tbz -- curl -s -m 1 http://curl.org 
+kubectl exec frontend-c74c5646c-c4tbz -- curl -s -m 1 http://curl.org | grep title
 kubectl exec frontend-c74c5646c-c4tbz -- curl -s -m 1 backend
 
 echo "From backend"
 kubectl exec backend-869fd89bdc-dppgm -- curl -s -m 1 frontend
-kubectl exec backend-869fd89bdc-dppgm -- curl -s -m 1 http://curl.org
+kubectl exec backend-869fd89bdc-dppgm -- curl -s -m 1 http://curl.org | grep title
